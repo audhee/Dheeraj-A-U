f3ddd4ef062508a7e83e7cf5d2fff5d3e914926c
Dheeraj-A-U <151025213+audhee@users.noreply.github.com>
2024-10-17 11:45:55 +0530

    Update input.dart
    I am learning 'Dart' so it is one of the basic program to take input from the user and printing it.

--- input.dart
+++ input.dart
@@ -1,10 +1,11 @@
 // TO taking input of the string and printing as the output
 
+// Basic program in dart to take input from the user.
 import 'dart:io';
 
-const Age = 45;
+const Age = 45;//Constant variable that cannot be changed
 void main() {
-  print('What is your name');
-  String? name = stdin.readLineSync();
-  print('Your Name is $name');
+  print('What is your name');// Printing "What is your name" in the screen
+  String? name = stdin.readLineSync();// Command to take input from the user part of the package i.e imported at the begging
+  print('Your Name is $name');//Printing the intaked command
 }
