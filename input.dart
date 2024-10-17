f7257c0ee8790e6e504aa27a6e7dafec42805060
Dheeraj-A-U <151025213+audhee@users.noreply.github.com>
2024-10-17 11:41:37 +0530

    Add files via upload

---
+++ input.dart
@@ -0,0 +1,10 @@
+// TO taking input of the string and printing as the output
+
+import 'dart:io';
+
+const Age = 45;
+void main() {
+  print('What is your name');
+  String? name = stdin.readLineSync();
+  print('Your Name is $name');
+}
